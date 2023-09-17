518a8789a36c4400b10dc345e136e3a46edf6ed8
NishimuraAtsuki <nishimura.atsuki1@gmail.com>
2023-09-17 16:55:19 +0900

    課題 1_22 提出

--- JavaCu1_22/src/execution/Qes1_22.java
+++ JavaCu1_22/src/execution/Qes1_22.java
@@ -1,12 +1,14 @@
 package execution;
 
+// パッケージのインポート
 import process.Introduce;
 
 public class Qes1_22 {
-	
 	public static void main(String[] args) {
+		// インスタンスの生成(引数として指定の値を渡す)
 		Introduce sushi = new Introduce("日本", "寿司", "和食");
 		
+		// メソッドの呼び出し
 		sushi.hello();
 		sushi.date();
 	}
--- JavaCu1_22/src/process/Introduce.java
+++ JavaCu1_22/src/process/Introduce.java
@@ -4,22 +4,27 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Introduce {
+	
+	// メンバ変数の作成
 	public String place;
 	public String food;
 	public String kind;
 	
+	// コンストラクタの作成
 	public Introduce(String place, String food, String kind) {
 		this.place = place;
 		this.food = food;
 		this.kind = kind;
 	}
 	
+	// 文字列を表示するメソッドの作成
 	public void hello() {
 		System.out.println("こんにちは!ここは" + this.place + "です!");
 		System.out.println("ここの" + this.food + "はうまい");
 		System.out.println(this.food + "は" + this.kind + "です");
 	}
 	
+	// 日付と時刻を出力するメソッドの作成
 	public void date() {
 		// オブジェクトを生成する
 		Calendar cl = Calendar.getInstance();
