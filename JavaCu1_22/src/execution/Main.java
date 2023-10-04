c44cebe0cd9faa002fa2a65687f454435a85f605
NishimuraAtsuki <nishimura.atsuki1@gmail.com>
2023-10-04 17:34:32 +0900

    課題1_30 新規作成

---
+++ JavaCu1_22/src/execution/Main.java
@@ -0,0 +1,19 @@
+package execution;
+
+import process.Person;
+
+class Main {
+	  public static void main(String[] args) {
+		// 人数を初期化する
+		Person.count = 0;
+	    Person person1 = new Person("鈴木太郎", 20, 1.7, 60.0);
+	    System.out.println(person1.name);
+	    System.out.println(person1.age);
+	    System.out.println(person1.height);
+	    System.out.println();
+	    // printメソッドの呼び出し
+	    person1.print();
+	    // 人数の出力
+	    System.out.println("人数は" + Person.count + "人です");
+	  }
+	}
--- JavaCu1_22/src/process/Person.java
+++ JavaCu1_22/src/process/Person.java
@@ -1,80 +1,30 @@
 package process;
 
-public class Person {
-	// メンバ変数の作成
-	// 名前
-	private String name;
-	// HP
-	private int hp;
-	// MP
-	private int mp;
-	// 攻撃力
-	private int attack;
-	// 素早さ
-	private int speed;
-	// 防御力
-	private int difence;
-	
-	// コンストラクタの作成
-		Person(String name, int hp, int mp, int attack, int speed, int difence) {
-			// メンバ変数に値をセットする
-			this.name = name;
-			this.hp = hp;
-			this.mp = mp;
-			this.attack = attack;
-			this.speed = speed;
-			this.difence = difence;
-		}
-		
-	// ゲッターメソッドの作成
-	public String getName() {
-		return this.name;
-	}
-	public int getHp() {
-		return this.hp;
-	}
-	public int getMp() {
-		return this.mp;
-	}
-	public int getAttack() {
-		return this.attack;
-	}
-	public int getSpeed() {
-		return this.speed;
-	}
-	public int getDifence() {
-		return this.difence;
-	}
-	
-	// セッターメソッドの作成
-	public void setName(String name) {
+public class Person{
+	// インスタンスフィールドを定義
+	public String name;
+	public int age;
+	public double height;
+	public double weight;
+	// 人数を表すインスタンスフィールドを定義
+	public static int count;
+	// コンストラクタを定義しインスタンスフィールドに値をセット
+	public Person(String name, int age, double height, double weight){
 		this.name = name;
-	}
-	public void setHp(int hp) {
-		this.hp = hp;
-	}
-	public void setMp(int mp) {
-		this.mp = mp;
-	}
-	public void setAttack(int attack) {
-		this.attack = attack;
-	}
-	public void setSpeed(int speed) {
-		this.speed =speed;
-	}
-	public void setDifence(int difence) {
-		this.difence = difence;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		// 人数を増やす
+		count++;
 	}
 	
-	// 出力用メソッドの作成
-	public void printData() {
-		System.out.println("\r\nこんにちは 「"  + this.name +" 」 さん");
-		System.out.println("ステータス");
-		System.out.println("HP：" + this.hp);
-		System.out.println("MP：" + this.mp);
-		System.out.println("攻撃力：" + this.attack);
-		System.out.println("素早さ：" + this.speed);
-		System.out.println("防御力：" + this.difence);
-		System.out.println("\r\nさあ冒険に出かけよう！");
+	// bmiメソッドの作成
+	public double bmi() {
+		return this.weight / this.height / this.height;
+	}
+	
+	// printメソッドの作成(BMIは四捨五入して出力する)
+	public void print() {
+		System.out.println("名前は" + this.name + "です\r\n年は" + this.age + "です\r\nBMIは" + Math.round(this.bmi()) + "です");
 	}
 }
